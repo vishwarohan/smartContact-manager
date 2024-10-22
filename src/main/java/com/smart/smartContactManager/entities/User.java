4a96439cdf2f8d09a9d7b0bbea0c9c87d1e46c04
PrathamVishk <impratham.vishk@gmail.com>
2024-10-22 22:58:25 +0530

    javax -> jakarta Fix and lombok added

--- src/main/java/com/smart/smartContactManager/entities/User.java
+++ src/main/java/com/smart/smartContactManager/entities/User.java
@@ -3,19 +3,21 @@ package com.smart.smartContactManager.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 
 @Entity
 @Table(name="USER")
+@Data
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -32,54 +34,6 @@ public class User {
 	private String about;
 	@OneToMany(cascade = CascadeType.ALL,fetch= FetchType.LAZY)
 	private List<Contact> contacts =new ArrayList<>();
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	public String getEnable() {
-		return enable;
-	}
-	public void setEnable(String enable) {
-		this.enable = enable;
-	}
-	public String getImageurl() {
-		return imageurl;
-	}
-	public void setImageurl(String imageurl) {
-		this.imageurl = imageurl;
-	}
-	public String getAbout() {
-		return about;
-	}
-	public void setAbout(String about) {
-		this.about = about;
-	}
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
